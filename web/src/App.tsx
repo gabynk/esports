ef0b6bea0f37ab495412d14b672e9cbd4c7b803c
Gabriele Nakassima <gaby.naka@gmail.com>
2022-09-19 19:26:42 -0300

    feat: add responsive design

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -32,11 +32,11 @@ export function App() {
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} alt="Logo ESports" />
 
-      <h1 className="text-6xl text-white font-black mt-20">
+      <h1 className="text-4xl md:text-6xl text-white font-black mt-20">
         Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui.
       </h1>
 
-      <div className="grid grid-cols-6 gap-6 mt-16">
+      <div className="grid grid-cols-3 md:grid-cols-6 gap-6 mt-16">
         {games.map(game => {
           return (
             <GameBanner
--- web/src/components/CreatedModal.tsx
+++ web/src/components/CreatedModal.tsx
@@ -55,8 +55,8 @@ export function CreatedModal() {
     <Dialog.Portal>
       <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
 
-      <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25">
-        <Dialog.Title className="text-3xl font-black">Publique um anúncio</Dialog.Title>
+      <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg max-w-[480px] shadow-lg shadow-black/25">
+        <Dialog.Title className="text-2xl md:text-3xl font-black">Publique um anúncio</Dialog.Title>
 
         <form onSubmit={handleCreatedAd} className="mt-8 flex flex-col gap-4">
           <div className="flex flex-col gap-2">
@@ -84,7 +84,7 @@ export function CreatedModal() {
             />
           </div>
 
-          <div className="grid grid-cols-2 gap-6">
+          <div className="grid md:grid-cols-2 gap-6">
             <div className="flex flex-col gap-2">
               <label htmlFor="yearsPlaying">Joga há quantos anos?</label>
               <Input
@@ -105,13 +105,13 @@ export function CreatedModal() {
             </div>
           </div>
 
-          <div className="flex gap-6">
+          <div className="flex flex-col md:flex-row gap-6">
             <div className="flex flex-col gap-2">
               <label htmlFor="weekDays">Quando costuma jogar?</label>
 
               <ToggleGroup.Root
                 type="multiple"
-                className="grid grid-cols-4 gap-2"
+                className="grid grid-cols-7 md:grid-cols-4 gap-2"
                 value={weekDays}
                 onValueChange={setWeekDays}
               >
